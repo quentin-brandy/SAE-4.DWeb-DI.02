f1af200991b1502242441f2c8da34a699951a2f5
Corentinpgt <corentin.pouget.19@gmail.com>
2024-02-20 11:15:53 +0100

    c'est bieng

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ function handlerMenu() {
 }
 
 btnMenu.addEventListener("click", handlerMenu);
+
+
+
+// imgToclick
+
+let divImg = document.querySelector("#imgLoaded");
+let imgContainer = document.querySelector(".imgToLoad");
+let imgs = document.querySelectorAll(".toClick");
+
+
+function handlerImg(ev) {
+    console.log(ev);
+    divImg.style.display = "block";
+    divImg.style.top = (window.innerHeight - ev.target.clientHeight) / 2 + "px";
+    imgContainer.src = ev.target.currentSrc;
+}
+
+imgs.forEach(img => {
+    img.addEventListener("click", handlerImg);
+});
+
+let btnCloseImg = document.querySelector(".toClose")
+
+function handlerCloseImg() {
+    divImg.style.display = "none";
+
+}
+
+btnCloseImg.addEventListener("click", handlerCloseImg);
